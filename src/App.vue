4e8fafc87b826ff47afc65f9a89f1e8e9462aa24
Tori Culler <tculler@ncsu.edu>
2021-05-19 16:01:28 -0400

    updatingheader

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <template>
   <head>
+    <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
     <meta name="viewport" content="width=device-width, initial-scale=1">
   </head>
+  <!-- <h1> The Thing From the Future </h1> -->
   <div id="app">
     <!-- <img  alt="The Thing From the Future logo" src="./assets/tftf_logo.jpeg" width="400"> -->
     <h1> The Thing From the Future </h1>
-    <br>
     <tftf msg="Wicked Problems, Wolfpack Solutions Edition"/>
   </div>
 </template>
@@ -22,6 +23,10 @@ export default {
 </script>
 
 <style>
+h1 {
+  font-family: 'Bebas Neue', cursive;
+  font-size: 400%; 
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
