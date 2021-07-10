b0e5a1ad1870f42406c371f156ca2f6635539c96
JoeMeister007 <joemeister007@gmail.com>
2021-07-10 12:53:51 -0400

    Added "Do Not Redeal" Checkmarks
    Implemented "do not redeal" checkmarks that appear after pressing Deal Cards for the first time. Checking one will prevent its respective card from being replaced upon pressing Deal Cards again. After hearing the instructions of remembering the card you wanted and pressing Deal Cards again if you liked one of your cards, but not the other, in the Wicked Problems, Wolfpack Solutions course, I decided to implement this to enhance the user experience.

--- src/components/tftf.vue
+++ src/components/tftf.vue
@@ -11,19 +11,38 @@
   </div>
 
   <div class="wrapper">
-    <div class="box a">
-      <p> ARC </p>
-      <h4>{{ arccards }}</h4>
+
+    <div class = "boxcontainer a">
+      <div class="box a">
+        <p> ARC </p>
+        <h4>{{ arccards }}</h4>
+      </div>
     </div>
-    <div class="box b">
-      <p> OBJECT </p>
-      <h4> there is a <br><em>{{ objectcards[0] }}</em></h4>
+
+    <div class = "boxcontainer b">
+      <div class="box b">
+        <p> OBJECT </p>
+        <h4> there is a <br><em>{{ objectcards[0] }}</em></h4>
+      </div>
+      <br>
+      <label class = "checkboxcontainer a"> Do Not Redeal
+        <input type="checkbox" id = "objectCheckBox">
+        <span class="checkmark"></span>
+      </label>
     </div>
-    <div class="box c">
-      <p> TERRAIN </p>
-      <h4> related to <br><em>{{ terraincards[0] }}</em>.</h4>
+
+    <div class = "boxcontainer c">
+
+      <div class="box c">
+        <p> TERRAIN </p>
+        <h4> related to <br><em>{{ terraincards[0] }}</em>.</h4>
+      </div>
+      <br>
+      <label class = "checkboxcontainer b"> Do Not Redeal
+        <input type="checkbox" id = "terrainCheckBox">
+        <span class="checkmark"></span>
+      </label>
     </div>
-    
 
     
   </div>
@@ -57,6 +76,7 @@
 </template>
 
 <script>
+var dealBeenClckd = false;
 export default {
   name: 'tftf',
   props: {
@@ -110,8 +130,19 @@ export default {
       });
     },
     deal_cards() {
-      this.objectcards[0] = this.objectcards[Math.floor(Math.random() * this.objectcards.length)]
-      this.terraincards[0] = this.terraincards[Math.floor(Math.random() * this.terraincards.length)]
+      if (!dealBeenClckd){ //makes the checkmarks visible after the first time the deal button has been clicked
+        dealBeenClckd = true;
+        var checkBoxes = document.getElementsByClassName("checkboxcontainer");
+        checkBoxes.forEach(element => element.style.display = "block");
+      }
+      //randomly selects another card if its respective checkbox is unchecked
+      if(!document.getElementById("objectCheckBox").checked){
+        this.objectcards[0] = this.objectcards[Math.floor(Math.random() * this.objectcards.length)]
+      }
+      if(!document.getElementById("terrainCheckBox").checked){
+        this.terraincards[0] = this.terraincards[Math.floor(Math.random() * this.terraincards.length)]
+      }
+      
     }
   }
 }
@@ -130,20 +161,23 @@ li {
     margin: 0 10px;
 }
 
-@media screen and (min-width: 601px){
+@media screen and (min-width: 630px){
   .wrapper {
     display: grid;
     grid-template-columns: repeat(3, minmax(0, 1fr));;
     background-color: #fff;
     color: rgb(202, 189, 189);
     max-width: 800px;
-    max-height: 300px;
+    max-height: 375px;
     margin: 0 auto; 
     padding: 25px; 
     column-gap: 15px;
     row-gap: 15px; 
   }
+  
   .box {
+    min-height: 250px;
+    max-height: 250px;
     background-color: #fff;
     color: black;
     border-style: solid;
@@ -155,27 +189,92 @@ li {
     box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
     text-align: center;
     }
-  .wrapper .box:nth-child(1) {    background:#8ec741;           }
-  .wrapper .box:nth-child(2) {    background:#00bef3;            }
-  .wrapper .box:nth-child(3) {    background:#f36d7d;         }
+
+  .checkboxcontainer{/*the container for the custom checkbox and its text*/
+    display:none;
+    position: relative;
+    padding-left:35px;
+    margin-bottom: 12 px;
+    cursor: pointer;
+    font-size: 22px;
+    color:black;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+  }
+
+  .checkboxcontainer input {/*hides the default checkbox so it can be replaced by the custom one*/
+    position: absolute;
+    opacity: 0;
+    cursor: pointer;
+    height: 0;
+    width: 0;
+  }
+
+  .checkmark {/*the custom checkbox*/
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 25px;
+    width: 25px;
+    background-color: #eee;
+    border-style: solid;
+    border-color: black;
+    border-width: 4px;
+    border-radius: 6px;
+  }
+
+  .checkmark:after{/*hides the actual check of the checkmark by default*/
+    content:"";
+    position: absolute;
+    display: none;
+  }
+
+  .checkboxcontainer input:checked ~ .checkmark:after{/*makes the actual check of the checkmark visible when the user clicks on the checkbox*/
+    display:block;
+  }
+
+  .checkboxcontainer .checkmark:after{ /*the check*/
+    left:4px;
+    top: 0px;
+    width: 9px;
+    height:16px;
+    border:solid white;
+    border-width: 0 3px 3px 0;
+    -webkit-transform: rotate(45deg);
+    -ms-transform: rotate(45deg);
+    transform: rotate(45deg);
+  }
+
+  .wrapper .boxcontainer:nth-child(1)  .box {    background:#8ec741;           }
+  .wrapper .boxcontainer:nth-child(2) .box {    background:#00bef3;            }
+  .wrapper .boxcontainer:nth-child(3) .box {    background:#f36d7d;         }
+  /*sets the background of the checkboxes to the background color of its corresponding boxcontainer*/
+  .wrapper .boxcontainer:nth-child(2)  .checkboxcontainer .checkmark {    background-color:#00bef3;           }
+  .wrapper .boxcontainer:nth-child(3) .checkboxcontainer .checkmark {    background-color:#f36d7d;            }       
+
 }
 
-@media screen and (max-width: 600px){
+@media screen and (max-width: 629px){
   .wrapper {
     display: grid;
     grid-template-rows: repeat(3, minmax(0, 1fr));
     background-color: #fff;
     color: rgb(202, 189, 189);
     max-width: 300px;
-    max-height: 800px;
+    max-height: 975px;
     margin: 0 auto; 
     padding: 25px; 
     column-gap: 15px;
     row-gap: 15px; 
   }
+
   .box {
     background-color: #fff;
     color: black;
+    min-height: 250px;
+    max-height: 250px;
     border-style: solid;
     border-color: black;
     border-radius: 23px;
@@ -185,8 +284,69 @@ li {
     box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
     text-align: center;
   }
-  .wrapper .box:nth-child(1) {    background:#8ec741;           }
-  .wrapper .box:nth-child(2) {    background:#00bef3;            }
-  .wrapper .box:nth-child(3) {    background:#f36d7d;         }
+
+  .checkboxcontainer{/*the container for the custom checkbox and its text*/
+    display:none;
+    position: relative;
+    padding-left:35px;
+    margin-bottom: 15 px;
+    cursor: pointer;
+    font-size: 22px;
+    color:black;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+  }
+
+  .checkboxcontainer input {/*hides the default checkbox so it can be replaced by the custom one*/
+    position: absolute;
+    opacity: 0;
+    cursor: pointer;
+    height: 0;
+    width: 0;
+  }
+
+  .checkmark {/*the custom checkbox*/
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 25px;
+    width: 25px;
+    background-color: #eee;
+    border-style: solid;
+    border-color: black;
+    border-width: 4px;
+    border-radius: 6px;
+  }
+
+  .checkmark:after{/*hides the actual check of the checkmark by default*/
+    content:"";
+    position: absolute;
+    display: none;
+  }
+
+  .checkboxcontainer input:checked ~ .checkmark:after{/*makes the actual check of the checkmark visible when the user clicks on the checkbox*/
+    display:block;
+  }
+
+  .checkboxcontainer .checkmark:after{/*the check*/
+    left:4px;
+    top: 0px;
+    width: 9px;
+    height:16px;
+    border:solid white;
+    border-width: 0 3px 3px 0;
+    -webkit-transform: rotate(45deg);
+    -ms-transform: rotate(45deg);
+    transform: rotate(45deg);
+  }
+
+  .wrapper .boxcontainer:nth-child(1)  .box {    background:#8ec741;           }
+  .wrapper .boxcontainer:nth-child(2) .box {    background:#00bef3;            }
+  .wrapper .boxcontainer:nth-child(3) .box {    background:#f36d7d;         }
+  /*sets the background of the checkboxes to the background color of its corresponding boxcontainer*/
+  .wrapper .boxcontainer:nth-child(2)  .checkboxcontainer .checkmark {    background-color:#00bef3;           }
+  .wrapper .boxcontainer:nth-child(3) .checkboxcontainer .checkmark {    background-color:#f36d7d;            } 
 }
 </style>
