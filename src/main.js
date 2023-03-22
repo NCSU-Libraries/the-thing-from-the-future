353d9f3313621a937c51fccf4da3ce3773da91b7
Niqui <doneill@ncsu.edu>
2023-03-22 15:41:06 -0400

    remove old dependency

--- src/main.js
+++ src/main.js
@@ -1,11 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import PapaParse from 'papaparse'
 // import lodash from 'lodash'
 
 
 createApp(App)
-    .provide('papaparse', PapaParse)
     // .provide('lodash', lodash)
     .mount('#app');
 
